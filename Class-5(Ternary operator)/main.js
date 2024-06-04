d86dbd3723bcf0ca3395c66855168e98ed037715
moriomnela <moriomislamnela23@gmail.com>
2024-06-05 00:22:57 +0600

    practice js ternary operator.

---
+++ Class-5(Ternary operator)/main.js
@@ -0,0 +1,8 @@
+let age = 40;
+let salary = 5000000;
+let height = 6;
+
+let beya =(age >= 30 && salary > 500000 && height > 6) || salary > 2000000
+?"Kobol"
+:"not Kobol";         
+console.log(beya);+ 22    
