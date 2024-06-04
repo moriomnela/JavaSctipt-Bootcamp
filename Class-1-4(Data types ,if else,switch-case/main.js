d24204514bc8992d3b7252c1e8ae3a5e1d879024
moriomnela <moriomislamnela23@gmail.com>
2024-06-05 00:20:09 +0600

    add js class.

---
+++ Class-1-4(Data types ,if else,switch-case/main.js
@@ -0,0 +1,149 @@
+  // let name ="Moriom Islam Nela";
+// let age =18;
+// let skill = "Web developer";
+
+// console.log (
+
+// `
+//    Name : ${name}
+//    Age : ${age}
+//    Skill: ${skill}
+
+
+
+// `
+// );
+
+// practise---2
+// let studentname =prompt("Student Name");
+// let birth_year =prompt ("Student birth year");
+
+// let age = 2024 -birth_year;
+
+// console.log(age);
+
+
+
+// //practise-------3
+// let studentname =prompt("Student Name");
+// let birth_year =prompt("Birth-year");
+
+// let age = 2024 - birth_year;
+
+// if (age>=4) {
+//    console.log (`hi ${studentname},you are okay Welcome to our school`);
+// }
+// else {
+//     console.log(`hi ${studentname},you are not allowed`);
+// }
+
+
+// let name = prompt ("Your Name");
+// let permission = confirm ("Is your father agree?");
+// let taka = prompt ("Taka");
+// let strong = confirm ("Are you strong?");
+
+
+// if ((permission == true && taka >=500 && strong == true) || taka >= 5000){
+//     console.log(`Hello ${name}, Come fast`);
+
+// }
+
+// else {
+//     console.log (`hello ${name},We are so sorry`);
+// }
+
+// practise----4
+
+let name = prompt ("Your Name");
+let birth_year = prompt ("Your Birth Year");
+let male = confirm ("Are you Male");
+
+let age = 2024 - birth_year;
+let marrige_age = 18;
+let somvodon = "Apu";
+
+if (male == true) {
+    beyar_age = 21;
+    somvodon = "Vaiya";
+}
+
+if (age >= 18) {
+    console.log(`hello ${name} ${somvodon}, You will get married`);
+}
+else {
+    console.log(`hello ${name +" "+somvodon},You are not of marrigeable age`);
+}
+
+//     practise-----5
+// let name = prompt ("Name");
+// let ban = prompt ("Bangla");
+
+
+// let gpa;
+// let grade;
+
+
+// if (ban >= 0 && ban < 33 ){
+//     gpa=0;
+//     grade="F";
+// }
+
+// else if (ban >= 33 && ban < 40) {
+//     gpa=1;
+//     grade="D";
+// }
+// else if (ban >= 40 && ban < 50){
+//     gpa=2;
+//     grade="C";
+// }
+// else if (ban >=50 && ban < 60){
+//     gpa = 3;
+//     grade = "B";
+// }
+// else if (ban >=60 && ban < 70){
+//     gpa = 3.5;
+//     grade = "A-";
+// }
+// else if (ban >=70 && ban < 80){
+//     gpa = 4;
+//     grade ="A";
+// }
+// else if (ban >=80 && ban < 100){
+//     gpa = 5;
+//     grade ="A+";
+// }
+// else {
+//     gpa = "jamela ase";
+//     grade = "jamela ase";
+// }
+// console.log (`
+//     Name: ${name}
+//     Marks: ${ban}
+//     Gpa: ${gpa}
+//     Grade: ${grade}
+//     `);
+     
+
+
+    // //  practise-----6 
+    //  let name = prompt ("Your name");
+    //  let dist = prompt ("Your dist");
+     
+    //  switch (dist) {
+    //     case "dha":
+    //       console.log (`abbe ${name} ,abbe bala aso ni`);
+    //       break;
+
+    //     case "syl":
+    //       console.log (`kita khobor ${name} , valo aso ni`);
+    //       break;
+    //     case "chg":
+    //       console.log (`kita khobor ${name} , valo aso ni`);
+    //       break;  
+    //     default:
+    //         console.log (` ${name} vai apni kon jalar manush`);
+    //         break;   
+    //  }
+  
+    
