8f3b6dfb262e25eedd2987680ce62bec2193f332
moriomnela <moriomislamnela23@gmail.com>
2024-06-05 23:23:28 +0600

    For while loop

---
+++ Class-6 (For,While,Do-WhileLoop)/main.js
@@ -0,0 +1,53 @@
+// let rifat = 10;
+
+// for (let nela = 0; nela <= rifat ; nela = nela + 2) {
+//     console.log (`Nelar amm =`+ nela);
+// }
+
+
+// for (let i = 500; i >= 300; i--){
+//      if (i % 2 == 0 ){
+//         console.log(i);
+//      }
+// }
+
+
+
+
+// for (let i = 0; i <= 100; i++){
+//     console.log(i);
+//     if (i == 100){
+//       for (let j = 100; j >= 0; ){
+//          console.log (j);
+//       }
+//     }
+// }
+
+
+  // 5,,7 Namota toiri
+  
+  for (let i = 1; i <= 10; i++){
+    console.log(`${i} ======== Ar Namota`)
+ for (let j = 1; j <= 10; i++){
+    console.log(`${i} * ${j} = ${i * j}`);
+
+ }  
+ }
+
+
+//While======
+//  let i = 0;
+// while(i <= 20 ){
+//    console.log(i);
+//    i++;
+// }
+
+
+
+//do-while=====
+// let i = 100;
+// do{
+//    console.log(i);
+//    i++;
+// }
+// while(i <= 20);
