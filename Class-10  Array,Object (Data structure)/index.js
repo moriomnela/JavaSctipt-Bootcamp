46b4b856144c910a4697bfbb67b38a8ac279f16c
moriomnela <moriomislamnela23@gmail.com>
2024-06-13 22:50:01 +0600

    Array,Object

---
+++ Class-10  Array,Object (Data structure)/index.js
@@ -0,0 +1,84 @@
+// const name = ["Rifat", "Nazrul", "Moriom", "raju", "Rokeya","lipi"];
+// const age = [14 ,47, 20, 27, 43, 30];
+// const location = ["USA" ,"Sirajganj" ,"London", "Japan", "Gazipur", "Sirajganj"];
+
+
+//    for(let i = 0; i < name.length; i++){
+//     if(location[i] =="Gazipur", "Sirajganj"){
+//         console.log(`Hello ${name[i]}`);
+
+//     }
+   
+// }
+
+
+// const food1 = ["alo", "potol", "lao", "komra"];
+// const food2 = ["alo", "potol", "lao", "komra"];
+
+
+// console.log(food1.sort());
+
+// const devs = [
+//     "Nela",
+//     "Moriom",
+//     "Rifat",
+//     "Nurmoni",
+//     "Lipi",
+//     "Nazrul"
+// ];
+
+
+
+// for (let i = 0; i < devs.length; i++){
+//     console.log(`hello ${devs[i]},How are you?`);
+// }
+
+           // practise
+
+// devs
+// .sort()
+// .reverse()
+// .forEach((item) => {
+//     console.log(`hello ${item},How are you?`);
+// })
+
+
+
+
+
+// Database Blood Donation
+
+
+// const name = [
+//     "Nela",
+//     "Moriom",
+//     "Rifat",
+//     "Nurmoni",
+//     "Lipi",
+//     "Nazrul"
+// ];
+// const age = [
+//     20,
+//     21,
+//     14,
+//     7,
+//     40,
+//     47,
+// ];
+// const group = [
+//     "A+",
+//     "AB+",
+//     "O+",
+//     "B+",
+//     "O-",
+//     "A+"
+// ];
+// name.forEach((item,index) => {
+//     console.log(`Hello ${item},You are ${age[index]} years old and Your Blood group is ${group[index]} `);
+// });
+
+
+
+
+
+
